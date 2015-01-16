63810d2cbeff1b9f5118fa7ac8a24b7ca706490e
whonut <dylan@physicynicism.com>
2015-01-16 20:06:36 +0000

    updated conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -18,7 +18,7 @@ import os
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-sys.path.insert(0, '/Users/dylan/Documents/Code/Python/pyoeis/pyoeis/')
+sys.path.insert(0, os.abspath('../pyoeis'))
 
 # -- General configuration ------------------------------------------------
 
